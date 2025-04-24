62125a62dc9bcc44a022e9cad4fc210ed854965d
João Henrique Alves dos Santos <jaozao77@gmail.com>
2025-04-24 14:21:17 -0300

    Update dijkstra.cpp

--- Graph/dijkstra.cpp
+++ Graph/dijkstra.cpp
@@ -1,10 +1,15 @@
+// graph deve ser modificado para vector<vector<pii>> graph(MAX);
+// Vértice inicial 's', que não possui pai na busca
+// Colocar retorno de distancia caso haja um vértice específico para o qual queremos saber a distância
+// caso contrário, deixar como está, e dist conterá a distância para todos os vértices a partir de 's'
+
 int dist[MAX], parent[MAX];
 
 void dijkstra(int s) {
     priority_queue<pii, vector<pii>, greater<pii>> pq;
     fill(dist, dist + n, INF);
+    fill(parent, parent + n, -1);
     dist[s] = 0;
-    parent[s] = -1;
     pq.emplace(0, s);
     while(!pq.empty()) {
         auto [du, u] = pq.top();
