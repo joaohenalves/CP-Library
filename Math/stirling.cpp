2b5dcabce31003e9924720ccff7dbe185550729d
João Henrique Alves dos Santos <jaozao77@gmail.com>
2025-05-10 12:39:13 -0300

    Update stirling.cpp

--- Math/stirling.cpp
+++ Math/stirling.cpp
@@ -3,3 +3,12 @@
 double stirling(int n){
     return log(2. * PI * n) * 0.5 + n * (log(n) - 1.) ;
 }
+
+// Ou melhor:
+
+ll stirling(ll n){
+    // expl(1.0L) = número de euler 2,7182...
+    if(n == 0) return 1;
+    long double pi = acosl(-1.L);
+    return (ll) (log10l(2.L * pi * n) * 0.5L + n * (log10l(n) - log10l(expl(1.0L)))) + 1;
+}
